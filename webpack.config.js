cee14c3de1daae43c3274d2a27731fec23e7aa60
Alvin Wen <alvinwen424@gmail.com>
2018-05-07 15:37:34 -0400

    created: webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-
+  entry: './app/main.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '*']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      }
+    ]
+  }
 }
