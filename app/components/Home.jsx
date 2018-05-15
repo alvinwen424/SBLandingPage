4816639fd987d1679176e22e6138651349600fd3
Alvin Wen <alvinwen424@gmail.com>
2018-05-14 23:30:12 -0400

    fix: material-ui form bug

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,40 +1,82 @@
 import React, { Component } from 'react'
-import TextField from 'material-ui/TextField';
+import TextField from 'material-ui/TextField'
 import DropDownMenu from 'material-ui/DropDownMenu'
 import MenuItem from 'material-ui/MenuItem'
+import RaisedButton from 'material-ui/RaisedButton'
 
 export default class Home extends Component {
   constructor(props){
     super(props)
     this.state = {
-      month:"Month",
-      day:"Day",
-      year:"year"
+      month:1,
+      day:1,
+      year:1,
+      gender:1
     }
   }
 
+  onChange = (type, e, index, value) => {
+    e.preventDefault()
+    this.setState({[type]: value})
+  }
+
   render(){
-    const {month, day, year} = this.state
+    const {month, day, year, gender} = this.state
+    const months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    const days = []
+    const years = []
+    for(var i = 1; i <= 31; i++){
+      days.push(i)
+    }
+    for(var i= 1960; i <=(new Date()).getFullYear(); i++){
+      years.push(i)
+    }
     return (
-      <div>
-        <TextField
-          hintText="First Name"
+      <div className="signup-form">
+        <div className="logo" >
+          <img src="Storybox_1.jpg" width="100px" height="100px"/>
+        </div>
+        <div className="form">
+          <TextField
+            className='form-textbox'
+            hintText="First Name"
+          />
+          <TextField
+            className='form-textbox'
+            hintText="Last Name"
+          /><br />
+          <TextField
+            className='form-textbox'
+            hintText="Email"
+          />
+          <TextField
+            className='form-textbox'
+            hintText="Confirm Email"
+          /><br />
+          <DropDownMenu value={gender} onChange={((e, index, value)=> this.onChange('gender', e, index, value))}>
+            <MenuItem value='1' primaryText="Male" />
+            <MenuItem value='2' primaryText="Female" />
+          </DropDownMenu><br />
+          <DropDownMenu  value={month} onChange={((e, index, value)=> this.onChange('month', e, index, value))}>
+            {months.map((eachMonth, index) => {
+              return <MenuItem key={eachMonth + index} value={index + 1} primaryText={eachMonth} />
+            })}
+          </DropDownMenu>
+          <DropDownMenu value={day} onChange={((e, index, value)=> this.onChange('day', e, index, value))}>
+            {days.map((eachDay, index) => {
+              return <MenuItem key={eachDay + index} value={index + 1} primaryText={eachDay} />
+            })}
+          </DropDownMenu>
+          <DropDownMenu value={year} onChange={((e, index, value)=> this.onChange('year', e, index, value))}>
+            {years.map((eachYear, index) => {
+              return <MenuItem key={eachYear + index} value={index + 1} primaryText={eachYear} />
+            })}
+          </DropDownMenu>
+        </div>
+        <RaisedButton
+          label='submit'
+          backgroundColor='#84CAEE'
         />
-        <TextField
-          hintText="Last Name"
-        /><br />
-        <TextField
-          hintText="Email"
-        /><br />
-        <DropDownMenu value={month} onChange={this.onChange}>
-          <MenuItem  />
-        </DropDownMenu>
-        <DropDownMenu value={day} onChange={this.onChange}>
-          <MenuItem  />
-        </DropDownMenu>
-        <DropDownMenu value={year} onChange={this.onChange}>
-          <MenuItem  />
-        </DropDownMenu>
       </div>
     )
   }
